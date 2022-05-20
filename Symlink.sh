51f35838f44421eda8d8936a2011743fcf8529e8
Adam Liter <io@adamliter.org>
2022-05-20 12:09:52 -0400

    Update config for macOS w/Apple Silicon & Doom Emacs
    I've decided to rename this repo from config-files to conf, so some
    things had to be changed to accommodate that. Moreover, I now have a
    machine running macOS on Apple Silicon, which required a few changes.
    Homebrew is now installed into /opt/homebrew for macOS machines running
    on Apple Silicon, so changes had to be made in order to accommodate
    that.
    
    Finally, I'm now really all in on using Doom Emacs. For a while, I had
    been maintaining a vanilla Emacs configuration alongside a Doom Emacs
    configuration, but it's time to really just abandon the vanilla Emacs
    configuration.

--- Symlink.sh
+++ Symlink.sh
@@ -1,81 +1,14 @@
 #!/usr/bin/env bash
-# -*- mode: sh; fill-column: 72 -*-
-#
-# -----------------------------------------------------------------------
-# Exiting and debugging
-# -----------------------------------------------------------------------
-### Exit when a command fails
-set -o errexit
-### Exit when a command in a series of pipes fails
-set -o pipefail
-### Exit when there is an undeclared variable
-set -o nounset
-### Trace what gets executed (for debugging)
-#set -o xtrace
-
-# -----------------------------------------------------------------------
-# Define a list of the files to be looped over
-# -----------------------------------------------------------------------
-FILES="bash_aliases \
-bash_completion \
-bash_profile \
-bash_prompt \
-bashrc \
-.emacs.d \
-gitconfig \
-gitignore_global \
-profile \
-tmux.conf \
-zlogin \
-zshrc \
-KeyBindings/DefaultKeyBinding.dict \
-MailMate/Security.plist \
-MailMate/Layouts/verticalThreadCorr.plist \
-MailMate/Layouts/verticalThreadTag.plist \
-MailMate/Layouts/widescreenThreadCorr.plist \
-MailMate/Layouts/widescreenThreadTag.plist \
-OpenPGP/gpg.conf \
-ssh"
-
-# -----------------------------------------------------------------------
-# Create directory for MailMate layouts
-# -----------------------------------------------------------------------
-echo "Creating directory for MailMate layouts"
-mkdir -p "${HOME}/Library/Application Support/MailMate/Resources/\
-Layouts/Mailboxes"
-
-# -----------------------------------------------------------------------
-# Create directory for keybindings
-# -----------------------------------------------------------------------
-echo "Creating directory for key bindings"
-mkdir -p "${HOME}/Library/KeyBindings"
-
-# -----------------------------------------------------------------------
-# Symlink all of the files
-# -----------------------------------------------------------------------
-echo "Symlinking configuration files and dotfiles based on the OS..."
-for i in $FILES; do
-    if [[ "${i}" == ".emacs.d" ]] ; then
-	## Put Emacs directory in the right place
-	ln -sf "${HOME}/config-files/${i}" "${HOME}"
-    elif [[ "${i}" == "ssh" ]]; then
-	ln -sf "${HOME}/config-files/${i}" "${HOME}/.${i}"
-    elif [[ "${i}" == "OpenPGP/gpg.conf" ]] ; then
-	## Put gpg.conf in the right place
-	ln -sf "${HOME}/config-files/${i}" "${HOME}/.gnupg"
-    elif [[ "${i}" == "MailMate/Security.plist" ]] ; then
-	## Put security config for MailMate in the right place
-	ln -sf "${HOME}/config-files/${i}" \
-	   "${HOME}/Library/Application Support/MailMate"
-    elif echo "${i}" | grep -q "MailMate/Layouts/" ; then
-	## Put Layout stuff in the right place
-	ln -sf "${HOME}/config-files/${i}" \
-	   "${HOME}/Library/Application Support/MailMate/Resources/Layouts/Mailboxes"
-    elif [[ "${i}" == "KeyBindings/DefaultKeyBinding.dict" ]] ; then
-	ln -sf "${HOME}/config-files/${i}" \
-	   "${HOME}/Library/KeyBindings"
-    else
-	## All other dotfiles
-	ln -sf "${HOME}/config-files/dotfiles/${i}" "${HOME}/.${i}"
-    fi
-done
+ln -sf "${HOME}/conf/bash/bash_profile" "${HOME}/.bash_profile"
+ln -sf "${HOME}/conf/bash/bashrc" "${HOME}/.bashrc"
+ln -sf "${HOME}/conf/bash/profile" "${HOME}/.profile"
+ln -sf "${HOME}/conf/git/gitconfig" "${HOME}/.gitconfig"
+ln -sf "${HOME}/conf/git/gitignore_global" "${HOME}/.gitignore_global"
+ln -sf "${HOME}/conf/git/gitattributes_global" "${HOME}/.gitattributes_global"
+mkdir -p ~/.config/htop
+ln -sf "${HOME}/conf/htop/htoprc" "${HOME}/.config/htop/htoprc"
+mkdir -p ~/.config/lab
+ln -sf "${HOME}/conf/lab/lab.toml" "${HOME}/.config/lab/lab.toml"
+ln -sf "${HOME}/conf/tmux/tmux.conf" "${HOME}/.tmux.conf"
+ln -sf "${HOME}/conf/npm/npmrc" "${HOME}/.npmrc"
+ln -sf "${HOME}/conf/conda/condarc" "${HOME}/.condarc"
--- bootstrap.sh
+++ bootstrap.sh
@@ -13,106 +13,163 @@ if [[ $OSTYPE == darwin* ]]; then
     BASH_OS_TYPE='macOS'
 elif [[ $OSTYPE == linux-gnu ]]; then
     BASH_OS_TYPE='Linux'
+    echo "This bootstrapping script does not currently support Linux ..."
+    exit 1
 elif [[ '$OS' == Windows* ]]; then
     BASH_OS_TYPE='Windows'
+    echo "This bootstrapping script does not currently support Windows ..."
+    exit 1
 else
     BASH_OS_TYPE='Unknown'
+    echo "Unknown operating system ..."
+    exit 1
+fi
+
+if [[ $BASH_OS_TYPE == 'macOS' ]]; then
+    if [[ $(uname -m) == arm* ]]; then
+        MAC_OS_TYPE='apple-silicon'
+    elif [[ $(uname -m) == x86_64 ]]; then
+        MAC_OS_TYPE='intel'
+    else
+        MAC_OS_TYPE='Unknown'
+    fi
 fi
 if [[ $BASH_OS_TYPE == macOS ]]; then
     if type brew >/dev/null 2>&1; then
         echo "Homebrew is already installed ..."
     else
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        echo "Installing Homebrew ..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        echo "Homebrew was successfully installed ..."
+        if [[ $MAC_OS_TYPE == 'apple-silicon' ]]; then
+            echo "Temporarily adding Homebrew to PATH for Apple Silicon Mac ..."
+            eval $(/opt/homebrew/bin/brew shellenv)
+        fi
+        echo "Tapping ralwaycat/emacsport ..."
+        brew tap railwaycat/emacsmacport
     fi
-    brew update && brew upgrade
-    brew tap caskroom/cask
-    brew cask install emacs
-    brew install git
-elif [[ $BASH_OS_TYPE == Linux ]]; then
-    sudo apt-get update -y && sudo apt-get upgrade -y
-    sudo apt-get install -y emacs
-    sudo apt-get install -y git
 fi
 if [ -f "${HOME}/.ssh/config" ]; then
     cp -H "${HOME}/.ssh/config" "${HOME}/.ssh/config.temp"
     rm "${HOME}/.ssh/config"
     mv "${HOME}/.ssh/config.temp" "${HOME}/.ssh/config"
 fi
-rm -rf "${HOME}/config-files"
-git clone github:adamliter/config-files.git "${HOME}/config-files"
-cd "${HOME}/config-files"
+if [ -d "${HOME}/conf" ]; then
+    echo "~/conf already exists; backing it up to ~/conf.bkp ..."
+    cp -r "${HOME}/conf" "${HOME}/conf.bkp"
+fi
+rm -rf "${HOME}/conf"
+git clone github:adamliter/conf.git "${HOME}/conf"
+cd "${HOME}/conf"
 git submodule update --init --remote --recursive
 
 git submodule foreach --recursive \
   'git checkout \
   $(git config -f $toplevel/.gitmodules submodule.$name.branch || \
-  echo master)'
-rm -rf "${HOME}/.emacs.d"
-ln -sn "${HOME}/config-files/emacs.d" "${HOME}/.emacs.d"
-emacs --batch --load="${HOME}/.emacs.d/init.el"
+  echo main)'
+
+cd "${HOME}"
+if [[ $BASH_OS_TYPE == macOS ]]; then
+    if xcode-select -p >/dev/null 2>&1; then
+        echo "XCode command line tools are already installed ..."
+    else
+        echo "Installing XCode command line tools ..."
+        xcode-select --install
+    fi
+fi
+if [[ $BASH_OS_TYPE == macOS ]]; then
+    if ([[ $MAC_OS_TYPE == 'apple-silicon' ]] && [ -f /opt/homebrew/bin/emacs ]) \
+        || ([[ $MAC_OS_TYPE == 'intel' ]] && [ -f /usr/local/bin/emacs ]); then
+       echo "Emacs was already installed with Homebrew ..."
+    else
+        echo "Installing Emacs ..."
+        brew install emacs-mac  --with-emacs-big-sur-icon --with-imagemagick \
+            --with-natural-title-bar --with-native-comp
+    fi
+fi
+rm -rf "${HOME}/.doom.d"
+ln -sn "${HOME}/conf/doom.d" "${HOME}/.doom.d"
+if ~/.emacs.d/bin/doom >/dev/null 2>&1; then
+    echo "Doom Emacs is arleady installed ..."
+else
+    echo "Installing dependencies for Doom Emacs ..."
+    if [[ $BASH_OS_TYPE == macOS ]]; then
+        brew install git ripgrep
+    fi
+    echo "Installing optional dependencies for Doom Emacs ..."
+    if [[ $BASH_OS_TYPE == macOS ]]; then
+        brew install coreutils fd
+    fi
+    echo "Cloning the doomemacs repository ..."
+    git clone https://github.com/doomemacs/doomemacs ~/.emacs.d
+    echo "Installing Doom Emacs ..."
+    ~/.emacs.d/bin/doom install
+fi
 emacs --batch \
   --eval="(progn
   (require 'org)
   (setq org-confirm-babel-evaluate nil)
-  (find-file \"~/config-files/README.org\")
+  (find-file \"~/conf/README.org\")
   (org-babel-tangle)
   (org-babel-execute-buffer)
   (kill-buffer))"
+. "${HOME}/conf/symlink.sh"
 if [[ $BASH_OS_TYPE == macOS ]]; then
-    brew update && brew install bash
-    if ! grep -q "/usr/local/bin/bash" /etc/shells; then
+    brew install bash
+    if ([[ $MAC_OS_TYPE == 'apple-silicon' ]] && ! grep -q "/opt/homebrew/bin/bash" /etc/shells) \
+        || ([[ $MAC_OS_TYPE == 'intel' ]] && ! grep -q "/usr/local/bin/bash" /etc/shells); then
         echo "Adding Homebrew's bash to possible login shells ..."
-        sudo bash -c "echo /usr/local/bin/bash >> /etc/shells"
+        if [[ $MAC_OS_TYPE == 'apple-silicon' ]]; then
+            sudo bash -c "echo /opt/homebrew/bin/bash >> /etc/shells"
+        fi
+        if [[ $MAC_OS_TYPE == 'intel' ]]; then
+            sudo bash -c "echo /usr/local/bin/bash >> /etc/shells"
+        fi
     else
         echo "Homebrew's bash is already a possible login shell ..."
     fi
-    if [ $SHELL == "/usr/local/bin/bash" ]; then
+    if ([[ $MAC_OS_TYPE == 'apple-silicon' ]] && [ $SHELL == "/opt/homebrew/bin/bash" ]) \
+        || ([[ $MAC_OS_TYPE == 'intel' ]] && [ $SHELL == "/usr/local/bin/bash" ]); then
         echo "Shell is already set to Homebrew's bash ..."
     else
         echo "Setting shell to Homebrew's bash ..."
-        chsh -s /usr/local/bin/bash
+        if [[ $MAC_OS_TYPE == 'apple-silicon' ]]; then
+            chsh -s /opt/homebrew/bin/bash
+        fi
+        if [[ $MAC_OS_TYPE == 'intel' ]]; then
+            chsh -s /usr/local/bin/bash
+        fi
     fi
 fi
-ln -sf "${HOME}/config-files/bash/bash_profile" "${HOME}/.bash_profile"
-ln -sf "${HOME}/config-files/bash/bashrc" "${HOME}/.bashrc"
-ln -sf "${HOME}/config-files/bash/profile" "${HOME}/.profile"
-if [[ $BASH_OS_TYPE == Linux ]]; then
-    sudo apt-get install -y curl
-fi
-ln -sf "${HOME}/config-files/git/gitconfig" "${HOME}/.gitconfig"
-ln -sf "${HOME}/config-files/git/gitignore_global" "${HOME}/.gitignore_global"
-ln -sf "${HOME}/config-files/git/gitattributes_global" "${HOME}/.gitattributes_global"
-if [[ $BASH_OS_TYPE == macOS ]]; then
-    brew install htop
-elif [[ $BASH_OS_TYPE == Linux ]]; then
-    sudo apt-get install -y htop
-fi
-#ln -sf "${HOME}/config-files/htop/htoprc" "${HOME}/.config/htop/htoprc"
-if [[ $BASH_OS_TYPE == macOS ]]; then
-    brew install pandoc
-elif [[ $BASH_OS_TYPE == Linux ]]; then
-    sudo apt-get install -y pandoc
-fi
-ln -sf "${HOME}/config-files/ssh/config" "${HOME}/.ssh/config"
-if [[ $BASH_OS_TYPE == macOS ]]; then
-    brew install tmux
-elif [[ $BASH_OS_TYPE == Linux ]]; then
-    sudo apt-get install -y tmux
-fi
-if [[ BASH_OS_TYPE == Linux ]]; then
-    if [ -f /usr/share/doc/tmux/examples/bash_completion_tmux.sh ]; then
-        echo "Symlinking tmux bash completion into place ..."
-        ln -sf "/usr/share/doc/tmux/examples/bash_completion_tmux.sh" "/etc/bash_completion.d/tmux"
-    else
-        echo "Could not find tmux bash completion file ..."
-    fi
-fi
-ln -sf "${HOME}/config-files/tmux/tmux.conf" "${HOME}/.tmux.conf"
-if [[ $BASH_OS_TYPE == Linux ]]; then
-    sudo apt-get install -y wget
-fi
-if [[ $BASH_OS_TYPE == macOS ]]; then
-    brew install reattach-to-user-namespace
-fi
+ln -sf "${HOME}/conf/bash/bashrc" "${HOME}/.bashrc"
+brew install cmake
+brew install editorconfig
+brew install git-lfs
+brew install gnupg
+brew install htop
+brew install hub
+brew install lab
+brew install pandoc
+brew install pass
+brew install pinentry-mac
+defaults write org.gpgtools.common UseKeychain NO
+defaults write org.gpgtools.common DisableKeychain -bool yes
+brew install reattach-to-user-namespace
+brew install ag
+ln -sf "${HOME}/conf/ssh/config" "${HOME}/.ssh/config"
+brew install tmux
+brew install wget
+brew install node
+brew install nodenv
+echo "Installing node package pyright globally ..."
+npm install -g pyright
 rm -rf "${HOME}/org"
-ln -sn "${HOME}/config-files/org" "${HOME}/org"
+ln -sn "${HOME}/conf/org" "${HOME}/org"
+brew install pyenv pyenv-virtualenv
+pyenv install 3:latest
+if [ -d "${HOME}/bin" ]; then
+    echo "~/bin already exists; backing it up to ~/bin.bkp ..."
+    cp -r "${HOME}/bin" "${HOME}/bin.bkp"
+fi
+rm -rf ${HOME}/bin
+ln -sn "${HOME}/conf/bin" "${HOME}/bin"
