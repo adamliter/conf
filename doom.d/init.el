e13869e1092f551d61b26ce4ba1e9a3f3a14b53e
Adam Liter <io@adamliter.org>
2025-09-03 17:28:29 -0600

    feat: enable Java and Kotlin editor support

--- doom.d/init.el
+++ doom.d/init.el
@@ -142,10 +142,10 @@
        ;;idris                                    ; a language you can depend on
        json                                       ; At least it ain't XML
        ;;janet                                    ; Fun fact: Janet is me!
-       ;;(java +lsp)                              ; the poster child for carpal tunnel syndrome
+       (java +lsp +tree-sitter)                   ; the poster child for carpal tunnel syndrome
        ;;javascript                               ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia                                    ; a better, faster MATLAB
-       ;;kotlin                                   ; a better, slicker Java(Script)
+       (kotlin +lsp +tree-sitter)                 ; a better, slicker Java(Script)
        latex                                      ; writing papers in Emacs has never been so fun
        ;;lean                                     ; for folks with too much to prove
        ;;ledger                                   ; be audit you can be
